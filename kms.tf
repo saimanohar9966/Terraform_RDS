ca139744f51b24dcf36bb10f9cd55887694465c6
saimanohar9966 <113921841+saimanohar9966@users.noreply.github.com>
2022-10-31 16:50:37 +0530

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -8,5 +8,5 @@ resource "aws_kms_key" "rds-key" {
 
 resource "aws_kms_alias" "rds-kms-alias" {
   target_key_id = aws_kms_key.rds-key.id
-  name          = "alias/rds-kms-key"
+  name          = "alias/rds-kms-keys"
 }
